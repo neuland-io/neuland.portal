7764a093ebcf1ece054ba2496502a2c3191fb90c
Stefan <stefan@rabota>
2014-08-24 19:13:25 +0200

    receive new users via JSON

--- src/neuland/portal/views/api.py
+++ src/neuland/portal/views/api.py
@@ -1,3 +1,5 @@
+import json
+
 from pyramid.response import Response
 from pyramid.view import view_config
 
@@ -16,3 +18,12 @@ users = Service(name='users', path='/users', description="User handling")
 def get_users(request):
     """Returns user data for a logged in user"""
     return {'Hello': 'World'}
+
+@users.post()
+def create_user(request):
+    """Registers a new user"""
+    try:
+        content = json.loads(request.body.decode(encoding='UTF-8'))
+    except ValueError:
+        return False
+    return True
