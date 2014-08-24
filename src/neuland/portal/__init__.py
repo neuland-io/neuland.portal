91c9a2aeb87791ed245a00af3c575c172d2da235
Stefan <stefan@rabota>
2014-08-24 17:21:24 +0200

    introduce a second api wsgi process

--- src/neuland/portal/__init__.py
+++ src/neuland/portal/__init__.py
@@ -7,15 +7,24 @@ from .models import (
     )
 
 
-def main(global_config, **settings):
-    """ This function returns a Pyramid WSGI application.
+def portal(global_config, **settings):
+    """ This function returns the Portal WSGI application.
+    """
+    config = Configurator(settings=settings)
+    config.include('pyramid_chameleon')
+    config.add_static_view('static', 'static', cache_max_age=3600)
+    config.add_route('home', '/')
+    config.scan('neuland.portal.views')
+    return config.make_wsgi_app()
+
+def api(global_config, **settings):
+    """ This function returns the API WSGI application.
     """
     engine = engine_from_config(settings, 'sqlalchemy.')
     DBSession.configure(bind=engine)
     Base.metadata.bind = engine
     config = Configurator(settings=settings)
     config.include('pyramid_chameleon')
-    config.add_static_view('static', 'static', cache_max_age=3600)
-    config.add_route('home', '/')
-    config.scan()
+    config.include('cornice')
+    config.scan('neuland.portal.api')
     return config.make_wsgi_app()
